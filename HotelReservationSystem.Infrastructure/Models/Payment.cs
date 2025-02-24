04ceb3e161a159a768f2bf2e2bec7e91506bcef3
Cristopher Alvarado <cristopheralvarado101@gmail.com>
2025-02-24 12:00:35 -0600

    Implement a payment model and an enun for payment methods

---
+++ HotelReservationSystem.Infrastructure/Models/Payment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using HotelReservationSystem.Infrastructure.Data.Enum;
+
+namespace HotelReservationSystem.Infrastructure.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int InvoiceId { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime PaymentDate { get; set; }
+
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; } = PaymentMethod.Other;
+
+        public Invoice Invoice { get; set; } = null!;
+    }
+}
