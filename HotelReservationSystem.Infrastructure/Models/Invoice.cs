22bb65c6af3e9cac76516be9fc197dddd558839e
Cristopher Alvarado <cristopheralvarado101@gmail.com>
2025-02-24 11:47:34 -0600

    Implements the Invoice model.

---
+++ HotelReservationSystem.Infrastructure/Models/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelReservationSystem.Infrastructure.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ReservationId { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalAmount { get; set; }
+
+        [Required]
+        public DateTime IssueDate { get; set; }
+
+        // Navigation Property
+        public Reservation Reservation { get; set; } = null!;
+    }
+}
