93cf6a4aeae6628127064b11c86eb4d23ad45772
Michelle Madrigal <madrigalmichelle52@gmail.com>
2025-02-28 20:45:04 -0600

    Fix for GetUserReservationHistoryAsync function

--- HotelReservationSystem.Core/Interfaces/IReservationService.cs
+++ HotelReservationSystem.Core/Interfaces/IReservationService.cs
@@ -1,10 +1,12 @@
 ﻿using HotelReservationSystem.Infrastructure.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HotelReservationSystem.Core.Interfaces
 {
     public interface IReservationService
     {
         Task<Reservation> ReserveRoomAsync(Reservation reservation);
-
+        Task<IEnumerable<Reservation>> GetUserReservationHistoryAsync(int userId);
     }
 }
