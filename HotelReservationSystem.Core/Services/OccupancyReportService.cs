fae19aeb9b0c3908f32a78b5df4bd65e4c581aee
Cristopher Alvarado <cristopheralvarado101@gmail.com>
2025-03-04 02:47:04 -0600

    Implements the service and repository for Occupancy Report, and also implements the unit test with the ids: TC-OR-001, TC-OR-002, TC-OR-003 for test OccupancyReportService.

---
+++ HotelReservationSystem.Core/Services/OccupancyReportService.cs
@@ -0,0 +1,33 @@
+﻿using HotelReservationSystem.Core.Interfaces;
+using HotelReservationSystem.Infrastructure.Interfaces;
+
+namespace HotelReservationSystem.Core.Services
+{
+    public class OccupancyReportService : IOccupancyReportService
+    {
+        private readonly IOccupancyReportRepository _occupancyReportRepository;
+
+        public OccupancyReportService(IOccupancyReportRepository occupancyReportRepository)
+        {
+            _occupancyReportRepository = occupancyReportRepository;
+        }
+
+        public async Task<Dictionary<string, double>> GenerateOccupancyReportAsync(DateTime startDate, DateTime endDate)
+        {
+            ValidateDates(startDate, endDate);
+
+            var occupancyData = await _occupancyReportRepository.GetOccupancyRateAsync(startDate, endDate);
+
+            return occupancyData ?? new Dictionary<string, double>();
+        }
+
+        private void ValidateDates(DateTime startDate, DateTime endDate)
+        {
+            if (startDate <= DateTime.Now.Date)
+                throw new ArgumentException("Start date must be today or in the future.");
+
+            if (startDate >= endDate)
+                throw new ArgumentException("Start date must be before end date.");
+        }
+    }
+}
